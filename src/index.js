a2189104a0791aa5cfd916ee2105efd61ac4973b
William Wong <william.wai.lim.wong@gmail.com>
2023-04-10 20:23:18 -0400

    tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    //console.log(e.target.elements.namedItem('new-task-description').value)
+    createNewTask(e.target.elements.namedItem('new-task-description').value);
+  })
 });
+
+function createNewTask(newTask){
+  let task = document.createElement('li');
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  task.textContent = `${newTask} `;
+  
+  document.getElementById('tasks').appendChild(task)
+  task.appendChild(btn)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
